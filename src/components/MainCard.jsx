92befc1fcbdacb9c495cf2727c189468ef7bcfc1
Parv Shrivastava <parvshrivastava@gmail.com>
2021-09-18 10:51:58 -0500

    Get user ID from Spotify API

--- src/components/MainCard.jsx
+++ src/components/MainCard.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import SpotifyWebApi from "spotify-web-api-js";
 
 function MainCard(props) {
-    
+
     return (
         <div className="MainCard">
             This is the main card with {props.access_token}
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -2,16 +2,18 @@ import React, {useEffect} from 'react'
 import MainCard from './MainCard'
 import SpotifyWebApi from 'spotify-web-api-js';
 function MainPage() {
-    const url = window.location;
-
     const access_token = new URLSearchParams(window.location.hash).get('#access_token');
     useEffect(() => {
-        var spotifyApi = new SpotifyWebApi();
+        const spotifyApi = new SpotifyWebApi();
         spotifyApi.setAccessToken(access_token);
         // get Elvis' albums, passing a callback. When a callback is passed, no Promise is returned
         let playlistObject = spotifyApi.getPlaylistTracks('37i9dQZF1DWWBHeXOYZf74', function (err, data) {
             if (err) console.error(err);
             else console.log('Playlist Tracks', data);
+        });
+        let userInfo = spotifyApi.getMe(null, function (err, data) {
+            if (err) console.error(err);
+            else console.log('User Info', data);
         });
     })
     return (
