7225ae610b4ed80350297ce2ecfda7623b5fa377
Parv Shrivastava <parvshrivastava@gmail.com>
2021-09-18 00:47:20 -0500

    Figured out how to return the access token

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import MainCard from './MainCard'
 function MainPage() {
-    
+    const url = window.location;
+
+    const access_token = new URLSearchParams(window.location.hash).get('#access_token');
+
     return (
         <div>
             <p style={{color: "white"}}>
-                
+                {access_token}
             </p>
             <MainCard/>
             <MainCard/>
