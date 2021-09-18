e19fddf0ab04feb16bdadb65e248f09b3408cdcc
Alan Duong <alanduong07@gmail.com>
2021-09-18 13:36:19 -0500

    don't remember, changed css and added react tinder

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,15 +1,29 @@
 import React, {useEffect, useState} from 'react'
 import MainCard from './MainCard'
 import SpotifyWebApi from 'spotify-web-api-js';
+import TinderCard from 'react-tinder-card';
 function MainPage() {
+    //get access token from url once. 
     const access_token = new URLSearchParams(window.location.hash).get('#access_token');
+    //init state for playlist tracks
     const [playlistTracks, setPlaylistTracks] = useState([]);
+    //init state for userID
     const [userID, setUserID] = useState("");
 
+
+    //react-tinder functions
+    const onSwipe = (direction) => {
+        console.log('You swiped: ' + direction)
+    }
+    const onCardLeftScreen = (myIdentifier) => {
+        console.log(myIdentifier + ' left the screen')
+    }
+
+    //functions ran constantly to get playlist
     useEffect(() => {
         const spotifyApi = new SpotifyWebApi();
         spotifyApi.setAccessToken(access_token);
-        // get Elvis' albums, passing a callback. When a callback is passed, no Promise is returned
+        
         let playlistTracks = spotifyApi.getPlaylistTracks('37i9dQZF1DWWBHeXOYZf74', function (err, data) {
             if (err) console.error(err);
             else {
