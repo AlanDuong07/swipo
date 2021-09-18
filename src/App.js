d15c8cd5d1216e833e4a40f466a423bbc9cdf3b8
Alan Duong <alanduong07@gmail.com>
2021-09-17 22:52:33 -0500

    installed react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import './App.scss';
 
 function App() {
-  
+
   
   return (
     <div className="App">
       
-      <header className="App-header">
-        Your mom
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
