5f82f7faeb10bc750b58374f4047d5fbd1029076
Jacob Pan <jacobypan@gmail.com>
2021-09-17 23:11:18 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.scss';
 import {BrowserRouter as Router, Route} from "react-router-dom";
+
 function App() {
 
-  return (
-    <Router>
-      <div className="App">
-        <Route path="/" exact component={loginButton} /> 
-        <Route path="/callback" exact component={mainPage} /> 
-      </div>
-    </Router>
-  );
+    return (
+        <div>
+            <h1>Swipo</h1>
+            <Router>
+                <div className="App">
+                    <Route path="/" exact component={loginButton}/>
+                    <Route path="/callback" exact component={mainPage}/>
+                </div>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
