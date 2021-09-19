b4cfe428054039552edb68a946a55e7e23b7ea03
Alan Duong <alanduong07@gmail.com>
2021-09-18 20:44:49 -0500

    small changes to genre page and top nav

--- src/components/PickGenrePage.jsx
+++ src/components/PickGenrePage.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-
+import TopNav from './TopNav';
+import BottomNav from './BottomNav';
 function PickGenrePage() {
     //get access token from url once. 
     const access_token = new URLSearchParams(window.location.hash).get('#access_token');
     return (
         <div id="PickGenrePage">
-            
+            <TopNav/>
+            <input type="text" placeholder="Search for a genre..."/>
+            <BottomNav/>
         </div>
     )
 }
