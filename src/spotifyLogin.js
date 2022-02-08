30fc1c5190488254f8d55c9e317bed6373547045
Alan Duong <alanduong07@gmail.com>
2022-02-08 13:29:35 -0600

    New swipoapp.com redirect url test

--- src/spotifyLogin.js
+++ src/spotifyLogin.js
@@ -3,7 +3,8 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 const clientId = "ba8410471b294344b293d5d6270ec54f";
 // const redirectUri = "http://localhost:3000/main/genrepicker/";
-const redirectUri = "https://swipo.onrender.com/main/genrepicker/";
+// const redirectUri = "https://swipo.onrender.com/main/genrepicker/";
+const redirectUri = "https://swipoapp.com/main/genrepicker/";
 //My Dorm Room IP
 // const redirectUri = "http://168.5.90.225:3000/main/genrepicker/";
 //Rice Library IP
